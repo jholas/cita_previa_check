6d5f424b83a23c2939d86487067412fabcf61506
jholas <jholas@hollees.org>
2025-06-10 18:01:14 +0200

    Update run.bat with example

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ REM check Enumerators from src/operation-enums to get values for PROVINCE, COUNT
 
 REM Example Residency
 REM npx ts-node src/index.ts -p "Alicante" --cita_op 22 --doc_type N --doc_num "NIE_NUM" --name "FIRSTNAME LASTNAME" --country "ALEMANIA" --birth-year 2010
+REM npx ts-node src/index.ts -p "Barcelona" --cita_op 4038 --doc_type N --doc_num "NIE_NUM" --name "FIRSTNAME LASTNAME" --country "ALEMANIA" --birth-year 2010
 
 REM Example NIE
 REM ts-node src/index.ts -p "Madrid" --cita_op 4031 --doc_type P --doc_num "PASSPORT_NUM" --name "FIRSTNAME LASTNAME" --country "BELGICA" --birth-year 1990
